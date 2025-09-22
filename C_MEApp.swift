5eeb3d9c94e89371f4226a1cfb75a24d0690a625
Maurice-SF <msf9314@nyu.edu>
2025-09-22 10:39:16 -0400

    Add files via upload

---
+++ C_MEApp.swift
@@ -0,0 +1,32 @@
+//
+//  C_MEApp.swift
+//  C-ME
+//
+//  Created by Maurice Farfan on 9/17/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct C_MEApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
